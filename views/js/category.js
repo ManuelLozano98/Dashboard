28302050f1f227388015a7a08013ea922a6ef2b8
ManuelLozano98 <lololozano1998@gmail.com>
2025-04-04 18:57:26 +0200

    insert categories and displaying table buttons

--- views/js/category.js
+++ views/js/category.js
@@ -1,61 +1,73 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 function init() {
-    show(false);
-    getCategories();
-
+  getCategories();
 }
 
-function show(bool) {
-    if (bool) {
-        $("#records").hide();
-        $("#addCategories").show();
-        $("#saveBtn").prop("disabled", false);
-        $("#addBtn").hide();
-    } else {
-        $("#records").show();
-        $("#addCategories").hide();
-        $("#addBtn").show();
-    }
+function insert() {
+  $.ajax({
+    url: "../controllers/category.php?do=save",
+    type: "POST",
+    data: {
+      name: $("#name").val(),
+      description: $("#description").val(),
+    },
+    success: function (result) {
+      toastr.success(result);
+      getCategories();
+    },
+  });
 }
 
 function getCategories() {
-    $("#tableCategories").DataTable(
-            {
-                buttons: [
-                    "copy",
-                    "csv",
-                    "excel",
-                    "pdf",
-                    "colvis"
-                ],
-                "ajax":
-                        {
-                            url: '../controllers/category.php?do=categories',
-                            type: 'get',
-                            dataType: "json",
-                        },
-                columns: [
-                    {"data": "id_category"},
-                    {"data": "name"},
-                    {"data": "description"},
-                    {"data": "active"}
-                ],
-                error: function (e) {
-                    console.log(e.responseText);
-                },
-                success: function (s) {
-                    console.log(s);
-                },
-                "bDestroy": true,
-                "iDisplayLength": 10,
-                "order": [[0, "desc"]],
-                "autoWidth": false,
-                "responsive": true
-            }).buttons().container('#tableCategories_wrapper .col-md-6:eq(0)');
+  $("#tableCategories").DataTable({
+    buttons: [
+      { extend: "copy", text: "Copy" },
+      { extend: "csv", text: "CSV" },
+      { extend: "excel", text: "Excel" },
+      { extend: "pdf", text: "PDF" },
+      { extend: "colvis", text: "Column visibility" },
+    ],
+    dom:
+      "<'row'<'col-md-4'l><'col-md-4 text-center'B><'col-md-4'f>>" + // Up: Select + Buttons + Search
+      "<'row'<'col-md-12'tr>>" + // Table
+      "<'row'<'col-md-6'i><'col-md-6'p>>", // Down: Info + Pagination
+
+    ajax: {
+      url: "../controllers/category.php?do=categories",
+      type: "get",
+      dataType: "json",
+    },
+    columns: [
+      { data: "id_category" },
+      { data: "name" },
+      { data: "description" },
+      { data: "active" },
+    ],
+    error: function (e) {
+      console.log(e.responseText);
+    },
+    success: function (s) {
+      console.log(s);
+    },
+    bDestroy: true,
+    iDisplayLength: 10,
+    order: [[0, "desc"]],
+    autoWidth: false,
+    responsive: true,
+  });
+}
+function updateCounter() {
+  const limit = 255;
+  $("#counter").text($("#description").val().length + "/" + limit);
+  if ($("#description").val().length >= limit) {
+    $("#counter").addClass("text-danger", "fw-bold");
+  } else {
+    $("#counter").removeClass("text-danger", "fw-bold");
+  }
 }
 init();
