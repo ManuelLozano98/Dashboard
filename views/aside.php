5142bcead970eec2bde886409e23bda31c0e8065
ManuelLozano98 <lololozano1998@gmail.com>
2025-04-14 01:34:07 +0200

    change url of the root path and categories

--- views/aside.php
+++ views/aside.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="./" class="brand-link">
         <img src="<?php echo $route; ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -35,7 +35,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="category.php" class="nav-link">
+                    <a href="categories" class="nav-link">
                         <i class="nav-icon fas fa-table"></i>
                         <p>
                             Categories
