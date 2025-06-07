28cbb345b36323b478ff38976f12ac2cacddcf8c
ManuelLozano98 <lololozano1998@gmail.com>
2025-06-07 17:16:17 +0200

    add articles view and url

--- views/aside.php
+++ views/aside.php
@@ -1,7 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="./" class="brand-link">
-        <img src="<?php echo $route; ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?php echo $route; ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -10,7 +11,8 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?php echo $route; ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?php echo $route; ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+                    alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">Alexander Pierce</a>
@@ -39,6 +41,14 @@
                         <i class="nav-icon fas fa-table"></i>
                         <p>
                             Categories
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="articles" class="nav-link">
+                        <i class="nav-icon fas fa-shop"></i>
+                        <p>
+                            Articles
                         </p>
                     </a>
                 </li>
