a5db5d17259447b0e1671afe1f257dc66797f73c
ManuelLozano98 <lololozano1998@gmail.com>
2025-06-11 21:51:19 +0200

    refactor the code
    - use of utils script
    - remove generic code

--- views/js/category.js
+++ views/js/category.js
@@ -28,16 +28,8 @@ function insert() {
   });
 }
 
-function deleteCategory(id) {
-  Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it"
-  }).then((result) => {
+function deleteItem(id) {
+  getDeleteMsg().then((result) => {
     if (result.isConfirmed) {
       $.ajax({
         url: `api/categories?id=${id}`,
@@ -73,17 +65,9 @@ function edit() {
 
 function getCategories() {
   $("#tableCategories").DataTable({
-    buttons: [
-      { extend: "copy", text: "Copy" },
-      { extend: "csv", text: "CSV" },
-      { extend: "excel", text: "Excel" },
-      { extend: "pdf", text: "PDF" },
-      { extend: "colvis", text: "Column visibility" },
-    ],
-    dom:
-      "<'row'<'col-md-4'l><'col-md-4 text-center'B><'col-md-4'f>>" + // Up: Select + Buttons + Search
-      "<'row'<'col-md-12'tr>>" + // Table
-      "<'row'<'col-md-6'i><'col-md-6'p>>", // Down: Info + Pagination
+    ...getSettingsDataTable(),
+    buttons: getButtonsDataTable(),
+    dom: getDomStyleDataTable(),
     ajax: {
       url: "api/categories",
       type: "get",
@@ -94,24 +78,8 @@ function getCategories() {
       { data: "name" },
       { data: "description" },
       { data: "active" },
-      {
-        data: null, // Column generated manually
-        title: "Actions",
-        orderable: false, // This column does not allow sorting
-        searchable: false, // This column does not allow searching
-        render: function (data, type, row) {
-          return `
-            <button id="btn-edit${data.id_category}" class="btn btn-success btn-sm rounded-0 edit-button" type="button" data-toggle="modal" data-target="#modal-edit-default" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
-            <button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete" onclick="deleteCategory(${data.id_category})"><i class="fa fa-trash"></i></button>
-          `;
-        },
-      },
-    ],
-    bDestroy: true,
-    iDisplayLength: 10,
-    order: [[0, "desc"]],
-    autoWidth: false,
-    responsive: true,
+      getActionsColumnDataTable(),
+    ]
   });
 }
 function updateCounter() {
@@ -136,14 +104,6 @@ function loadEditForm() {
     $("#edit-idcategory").val(data.id_category);
     $("#edit-description").text(data.description);
     data.active === "0" ? $("#customSwitch1").prop("checked", false) : $("#customSwitch1").prop("checked", true);
-  });
-}
-
-function closeModalDialog() {
-  $(document).on('keydown', function (event) {
-    if (event.key === "Escape") {
-      $('.modal').modal('hide');
-    }
   });
 }
 
@@ -188,4 +148,3 @@ function getErrorResponse(xhr) {
 }
 
 init();
-closeModalDialog();
