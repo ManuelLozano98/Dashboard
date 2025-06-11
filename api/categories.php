cb8199b34d0fdde23ce633649009e08d0a24ff5d
ManuelLozano98 <lololozano1998@gmail.com>
2025-06-11 21:55:53 +0200

    add endpoint getCategoriesName

--- api/categories.php
+++ api/categories.php
@@ -1,14 +1,18 @@
 <?php
 require_once "../controllers/category.php";
 $method = $_SERVER["REQUEST_METHOD"];
-$body = json_decode(file_get_contents('php://input'),true);
+$body = json_decode(file_get_contents('php://input'), true);
 switch ($method) {
     case 'GET':
-        getCategories();
+        if (isset($_GET["getCategoriesName"])) {
+            getCategoriesName();
+        } else {
+            getCategories();
+        }
         break;
     case 'POST':
     case 'PUT':
-        save($method,$body);
+        save($method, $body);
         break;
     case 'DELETE':
         deleteCategory($_REQUEST);
