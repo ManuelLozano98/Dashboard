88675e3380130cd88c229c6cb5f97549e13bb11a
ManuelLozano98 <lololozano1998@gmail.com>
2025-06-17 17:39:10 +0200

    add a method GET
    - set query params a POST values if the method isnt GET
    - if the method is GET and user sends the param code checks if exists or not

--- api/articles.php
+++ api/articles.php
@@ -1,14 +1,20 @@
 <?php
 require_once "../controllers/article.php";
 $method = $_SERVER["REQUEST_METHOD"];
-$body = json_decode(file_get_contents('php://input'),true);
+$body = json_decode(file_get_contents('php://input'), true);
+$queryParams = $_POST;
 switch ($method) {
     case 'GET':
-        getArticles();
+        $queryParams = $_GET;
+        if (!empty($queryParams["code"])) {
+            checkIfExitsCode($queryParams["code"]);
+        } else {
+            getArticles();
+        }
         break;
     case 'POST':
     case 'PUT':
-        save($method,$body);
+        save($method, $queryParams);
         break;
     case 'DELETE':
         deleteArticle($_REQUEST);
