75300bcba2ac31fac10465d20d57d0b591f4526c
ManuelLozano98 <lololozano1998@gmail.com>
2025-06-26 18:21:17 +0200

    add user login endpoint

---
+++ api/users.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../controllers/user.php";
+$method = $_SERVER["REQUEST_METHOD"];
+$body = json_decode(file_get_contents('php://input'), true);
+$queryParams = $_POST;
+switch ($method) {
+    case 'GET':
+        break;
+    case 'POST':
+        if ($_GET["q"] === "login") {
+            login($queryParams);
+        }
+        break;
+    case "PUT":
+
+        break;
+    case 'DELETE':
+        break;
+}
