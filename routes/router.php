3ed1d8014df2efbb6ea1d71a45ec866ecb80be4b
ManuelLozano98 <lololozano1998@gmail.com>
2025-07-17 18:48:30 +0200

    add router to delegate requests to controllers or API based on the route

---
+++ routes/router.php
@@ -0,0 +1,10 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (str_starts_with($uri, "/AdminDashboard/api/")) {
+    require_once __DIR__ . "/apiRouter.php";
+} else {
+    require_once __DIR__ . "/controllerRouter";
+}
+
