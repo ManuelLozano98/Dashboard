2f16b1b50ae2d3cb07f0e3d68a8dcabb34a5d897
ManuelLozano98 <lololozano1998@gmail.com>
2025-07-18 15:53:33 +0200

    add .php extension

--- routes/router.php
+++ routes/router.php
@@ -5,6 +5,6 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if (str_starts_with($uri, "/AdminDashboard/api/")) {
     require_once __DIR__ . "/apiRouter.php";
 } else {
-    require_once __DIR__ . "/controllerRouter";
+    require_once __DIR__ . "/controllerRouter.php";
 }
 
