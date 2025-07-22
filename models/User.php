1bada3165c64921d3b9dd724fa0d7be41139f1c6
ManuelLozano98 <lololozano1998@gmail.com>
2025-07-22 21:31:20 +0200

    refactor: model User
    - Move business logic from model to service
    - Update constructor fields
    - Add toArray method

--- models/User.php
+++ models/User.php
@@ -11,34 +11,31 @@
  *
  * @author Usuario
  */
-require_once '../configurations/database.php';
-require_once 'Response.php';
-require_once 'Mail.php';
+require_once __DIR__ . '/../configurations/database.php';
 
 
 class User
 {
-    private $id;
-    private $name;
-    private $username;
-    private $email;
-    private $password;
-    private $phone;
-    private $image;
-    private $address;
-    private $document;
-    private $document_type;
-    private $token;
-    private $tokenExpiredAt;
+    private ?int $id;
+    private string $name;
+    private string $username;
+    private string $email;
+    private string $password;
+    private string $phone;
+    private string $image;
+    private string $address;
+    private string $document;
+    private int $document_type;
+    private string $token;
+    private string $tokenExpiredAt;
 
-    private $active;
-    private $registration_date;
+    private int $active;
+    private string $registration_date;
 
-    private Response $response;
 
     public function __construct($data = []) // If there is data, it is filled with the parameters provided; if not, it is set to null
     {
-        $this->id = $data['id'] ?? "";
+        $this->id = $data['id_user'] ?? NULL;
         $this->name = $data['name'] ?? "";
         $this->username = $data['username'] ?? "";
         $this->email = $data['email'] ?? "";
@@ -51,303 +48,99 @@ class User
         $this->active = $data['active'] ?? 0;
         $this->token = $data['verification_token'] ?? "";
         $this->tokenExpiredAt = $data['token_expires_at'] ?? "";
-        $this->registration_date = (new Datetime("now"))->format('Y-m-d H:i:s');
+        $this->registration_date = empty($data['registration_date']) ? (new Datetime("now"))->format('Y-m-d H:i:s') : $data['registration_date'];
     }
 
-    public function getAll()
+    public static function getAll()
     {
-        $sql = "SELECT ID_USER,NAME,EMAIL,USERNAME,PHONE,IMAGE,ADDRESS,DOCUMENT,ID_DOCUMENT_TYPE,ACTIVE,VERIFICATION_TOKEN,TOKEN_EXPIRES_AT,REGISTRATION_DATE FROM USERS";
-        $records = $this->getCountUsers();
+        $sql = "SELECT * FROM USERS";
         $query = querySQL($sql);
-        $data = array(
-            "records" => (int) $records[0]["RECORDS"],
-            "data" => $query
-        );
-        return $data;
-    }
-
-    private function generateToken(User $user)
-    {
-        $user->setToken(bin2hex(random_bytes(32)));
-        $user->setTokenExpiredAt((new DateTime('+1 day'))->format('Y-m-d H:i:s'));
-    }
-
-    public function checkLogin($params)
-    {
-        $response = new Response();
-        $login = $params["login"];
-        $password = $params["password"];
-
-        $data = $this->isEmail($login)
-            ? $this->findByEmail($login)
-            : $this->findByUsername($login);
-
-        if ($data && $data["active"] === 1 && $this->checkPassword($password, $data["password"])) {
-            $response->getValidLoginMessage();
-        } else {
-            $response->getInvalidLoginMessage();
+        $users = [];
+        foreach ($query as $user) {
+            $users[] = new User($user);
         }
-
-        return $response;
-    }
-    private function checkPassword($password, $hash)
-    {
-        return password_verify($password, $hash);
+        return $users;
     }
 
-    private function isEmail($param)
+    public static function insert(User $user)
     {
-        return filter_var($param, FILTER_VALIDATE_EMAIL) !== false;
-    }
-
-    private function insert($user)
-    {
-        $this->response = new Response();
-        if ($this->findByUsername($user["username"]) || $this->findByEmail($user["email"])) {
-            $this->response->getAddConflictMessage("User");
-            return $this->response;
-
-        }
-        $hash = password_hash($user["password"], PASSWORD_BCRYPT);
-        $newUser = new User($user);
-        $newUser->setPassword($hash);
-        $this->generateToken($newUser);
         $sql = "INSERT INTO USERS VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        if (preparedQuerySQLObject($sql, "issssssssiisss", $newUser, ["id", "name", "email", "password", "username", "phone", "image", "address", "document", "document_type", "active", "token", "tokenExpiredAt", "registration_date"])) {
-            $this->response->getCreatedSuccesfullyMessage("User");
-            $this->response->setData($newUser->getUserCreatedInfo());
-        } else {
-            $this->response->setMessage("An error occurred, registration could not be completed");
-        }
-        if (Mail::sendEmailVerification($newUser->getEmail(), $newUser->getUsername(), $newUser->getToken())) {
-            $this->response->setDetails(["email" => "Email sent successfully"]);
-        } else {
-            $this->response->setDetails(["email" => "Email could not be sent. Mailer Error"]);
-        }
-
-        return $this->response;
+        return preparedQuerySQLObject($sql, "issssssssiisss", $user, ["id", "name", "email", "password", "username", "phone", "image", "address", "document", "document_type", "active", "token", "tokenExpiredAt", "registration_date"]) ? $user : false;
     }
 
-    private function saveImage($img)
+
+    public static function activateAccount(User $user)
     {
-        $tmpName = $img['tmp_name'];
-        $fileName = basename($img['name']);
-        $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
-
-        $allowed = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
-        if (in_array($fileExt, $allowed)) {
-            $newName = uniqid() . '.' . $fileExt;
-            $uploadPath = '../files/users_img/' . $newName;
-
-            if (move_uploaded_file($tmpName, $uploadPath)) {
-                return $newName;
-            } else {
-                return "File couldnt move";
-            }
-        } else {
-            return "Ext not allowed";
-        }
+        $sql = "UPDATE USERS SET ACTIVE = ?, VERIFICATION_TOKEN = ? WHERE ID_USER = ?";
+        return preparedQuerySQL($sql, "isi", 1, NULL, $user->getId());
     }
 
-    public function edit($data)
+    public static function edit(User $user)
     {
-        $response = new Response();
-        $dbUser = $this->findById($data["id_user"]);
-        if (!$dbUser) {
-            return $response->getUpdatedNotFoundMessage("User");
-        }
-        $emailTaken = $this->findByEmail($data["email"]);
-        if ($emailTaken) {
-            if (($emailTaken["email"] === $data["email"] && $dbUser["id_user"] !== $emailTaken["id_user"])) {
-                $response->getAddConflictMessage("user");
-                return $response;
-            }
-        }
 
-        $img = $dbUser["image"];
-        if (isset($_FILES["image"]) && $_FILES["image"]['error'] === UPLOAD_ERR_OK) {
-            $img = $this->saveImage($_FILES["image"]);
-        }
-
-        $set = "";
-        $types = "";
-        $password = "";
-        $email = "";
-
-        $user = new User($data);
-        $user->setId($dbUser["id_user"]);
-        $user->setImage($img);
-
-        if ($data["password"] !== "") {
-            $set = "PASSWORD=?, ";
-            $types = "s";
-            $password = "password";
-            $hash = password_hash($data["password"], PASSWORD_BCRYPT);
-            $user->setPassword($hash);
-
-        }
-        if (!empty($data["email"] && $data["email"] !== $dbUser["email"])) {
-            $set .= "EMAIL=?, ";
-            $types .= "s";
-            $email = "email";
-        }
-
-        $sql = "UPDATE USERS SET NAME=?, " . $set . "PHONE=?, IMAGE=?, ADDRESS=?, DOCUMENT=?, ID_DOCUMENT_TYPE=?, ACTIVE=? WHERE ID_USER=?";
-        $properties = ["name"];
-        if ($password !== "")
-            array_push($properties, $password);
-        if ($email !== "")
-            array_push($properties, $email);
-        array_push($properties, "phone", "image", "address", "document", "document_type", "active", "id");
-
-        if (preparedQuerySQLObject($sql, "s" . $types . "ssssiii", $user, $properties)) {
-            $response->getUpdatedSuccesfullyMessage("User");
-            $response->setData($user);
-        } else {
-            $response->getUpdatedExistsMessage("users");
-        }
-        return $response;
-
+        $sql = "UPDATE USERS SET NAME=?, EMAIL=?, PASSWORD=?, PHONE=?, IMAGE=?, ADDRESS=?, DOCUMENT=?, ID_DOCUMENT_TYPE=?, ACTIVE=? WHERE ID_USER=?";
+        return preparedQuerySQLObject($sql, "sssssssiii", $user, ["name", "email", "password", "phone", "image", "address", "document", "document_type", "active", "id"]) ? $user : false;
     }
 
-    public function delete($id)
+    public static function delete($id)
     {
-        $dbUser = $this->findById($id);
-        $this->response = new Response();
-        if (!$dbUser) {
-            $this->response->getDeletedNotFoundMessage("User");
-            return $this->response->buildResponse();
-        }
         $sql = "DELETE FROM USERS WHERE ID_USER = ?";
-        preparedQuerySQL($sql, "i", $id);
-        $this->response->getDeletedSuccesfullyMessage("User");
-        $this->response->setData(null);
-        return $this->response->buildResponse();
+        return preparedQuerySQL($sql, "i", $id);
     }
 
-    public function findById($id)
+    public static function findById($id)
     {
         $sql = "SELECT * FROM USERS WHERE ID_USER=?";
         $data = getDataPreparedQuerySQL($sql, "i", $id);
-        return $data ? $data[0] : null;
+        return !empty($data) ? new User($data[0]) : false;
+
     }
-    public function findByUsername($username)
+    public static function findByUsername($username)
     {
-        $sql = "SELECT * FROM USERS WHERE USERNAME=?";
+        $sql = "SELECT * FROM USERS WHERE USERNAME = ?";
         $data = getDataPreparedQuerySQL($sql, "s", $username);
-        return $data ? $data[0] : null;
+        return !empty($data) ? new User($data[0]) : false;
     }
-    public function findByEmail($email)
+    public static function findByEmail($email)
     {
         $sql = "SELECT * FROM USERS WHERE EMAIL=?";
         $data = getDataPreparedQuerySQL($sql, "s", $email);
-        return $data ? $data[0] : null;
+        return !empty($data) ? new User($data[0]) : false;
     }
-    public function findByPhone($phone)
+    public static function findByPhone($phone)
     {
         $sql = "SELECT * FROM USERS WHERE PHONE=?";
         $data = getDataPreparedQuerySQL($sql, "s", $phone);
-        return $data ? $data[0] : null;
+        return !empty($data) ? new User($data[0]) : false;
     }
-    public function findByDocument($document)
+    public static function findByDocument($document)
     {
         $sql = "SELECT * FROM USERS WHERE DOCUMENT=?";
         $data = getDataPreparedQuerySQL($sql, "s", $document);
-        return $data ? $data[0] : null;
+        return !empty($data) ? new User($data[0]) : false;
     }
-    public function findByToken($token)
+    public static function findByToken($token)
     {
         $sql = "SELECT * FROM USERS WHERE VERIFICATION_TOKEN=?";
         $data = getDataPreparedQuerySQL($sql, "s", $token);
-        return $data ? $data[0] : null;
-    }
-    public function verifyToken($token)
-    {
-        $response = new Response();
-        $response->setMessage("The token has already expired");
-        $response->setStatus(409);
-        $user = $this->findByToken($token);
-        $date = (new DateTime('now'))->format('Y-m-d H:i:s');
-        if ($user && !$user["active"] && $user["token_expires_at"] > $date) {
-            $sql = "UPDATE USERS SET ACTIVE = 1, VERIFICATION_TOKEN = NULL WHERE ID_USER = ?";
-            if (preparedQuerySQL($sql, "i", $user["id_user"])) {
-                $response->setMessage("The user has been successfully verified");
-                $response->setStatus(201);
-
-            }
-        }
-        return $response;
+        return !empty($data) ? new User($data[0]) : false;
     }
 
-    public function getUserCreatedInfo()
-    {
-        return [
-            "name" => $this->getName(),
-            "username" => $this->getUsername(),
-            "email" => $this->getEmail()
-        ];
-    }
 
-    public function getCountUsers()
+    public static function getCountUsers()
     {
         $sql = "SELECT COUNT(*) AS RECORDS FROM USERS";
         return querySQL($sql);
     }
-    public function findByName($name)
+    public static function findByName($name)
     {
         $sql = "SELECT * FROM USERS WHERE NAME = ?";
         $data = getDataPreparedQuerySQL($sql, "s", $name);
-        return $data;
-    }
-
-    public function insertOrEdit($data, $method)
-    {
-        $flag = $this->validateFields($data, $method);
-        if ($flag === TRUE) {
-            if ($method === "POST") {
-                return $this->insert($data);
-            } else {
-                return $this->edit($data);
-            }
+        $users = [];
+        foreach ($data as $user) {
+            $users[] = new User($user);
         }
-        return $flag;
-    }
-    private function validateFields($fields, $method)
-    {
-        $response = new Response();
-        $response->getRequiredFieldsMissingMessage();
-        if ($method === "POST") {
-            $requiredFields = ["name", "email", "password", "username"];
-            $validatedFields = 0;
-            foreach ($requiredFields as $field) {
-                if (array_key_exists($field, $fields) && $fields[$field] !== "") {
-                    $validatedFields++;
-                } else {
-                    $response->setDetails([$field => "This field is required"]);
-                }
-
-            }
-            if ($validatedFields === count($requiredFields)) {
-                return true;
-            }
-            return $response->buildResponse();
-        } else {
-            $requiredFields = ["id_user"];
-            $validatedFields = 0;
-            foreach ($requiredFields as $field) {
-                if (array_key_exists($field, $fields) && $fields[$field] !== "") {
-                    $validatedFields++;
-                } else {
-                    $response->setDetails([$field => "This field is required"]);
-                }
-
-            }
-            if ($validatedFields === count($requiredFields)) {
-                return true;
-            }
-
-            return $response->getDetails() ? $response->buildResponse() : true;
-        }
+        return $users;
     }
 
     /**
@@ -627,6 +420,26 @@ class User
         $this->registration_date = $registration_date;
 
         return $this;
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'username' => $this->username,
+            'phone' => $this->phone,
+            'image' => $this->image,
+            'address' => $this->address,
+            'document' => $this->document,
+            'document_type' => $this->document_type,
+            'active' => $this->active,
+            'verification_token' => $this->token,
+            'tokenExpiredAt' => $this->tokenExpiredAt,
+            'registration_date' => $this->registration_date,
+        ];
+
     }
 }
 
