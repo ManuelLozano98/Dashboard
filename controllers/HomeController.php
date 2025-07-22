7d066fb7e8c125b7ce55d984515e24b1cc506526
ManuelLozano98 <lololozano1998@gmail.com>
2025-07-22 17:44:58 +0200

    add user service to the controller and confirm email using token

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -5,12 +5,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+require_once __DIR__ . '/../services/UserService.php';
 
 class HomeController
 {
+    private UserService $userService;
     public function __construct()
     {
+        $this->userService = new UserService();
 
     }
 
@@ -36,8 +38,21 @@ class HomeController
     }
     public function emailConfirmed()
     {
-        require VIEWS_PATH . '/email-confirmed.php';
+        $token = $_GET['token'] ?? null;
+        $userParam = $_GET['user'] ?? null;
+
+        if ($token) {
+            $result = $this->userService->verifyToken($token);
+            if ($result->getStatus() === 200) {
+                require VIEWS_PATH . '/email-confirmed.php';
+                return;
+            }
+        }
+        if ($userParam === 'not-registered') {
+            require VIEWS_PATH . '/email-confirmed.php';
+            return;
+        }
+        header("Location: /AdminDashboard/email/confirm?user=not-registered");
+        exit;
     }
-
-
 }
