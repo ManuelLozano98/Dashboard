74665d3a45cc88dbbb8d84ac59e8e7d96ee56b2a
ManuelLozano98 <lololozano1998@gmail.com>
2025-08-04 22:25:58 +0200

    add login state logic and admin routing
    - Define route access for admin users
    - Detect when a user is logged in
    - Show different home page content based on login status
    - Add logout route behavior depending on user state

--- routes/controllerRouter.php
+++ routes/controllerRouter.php
@@ -1,13 +1,36 @@
 <?php
 require_once __DIR__ . '/../configurations/config.php';
-
+require_once __DIR__ . '/../controllers/HomeController.php';
+require_once __DIR__ . '/../services/UserRolesService.php';
 define('VIEWS_PATH', dirname(__DIR__) . '/views');
 $method = $_SERVER["REQUEST_METHOD"];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uriParts = array_values(array_filter(explode('/', $uri)));
-$resource = $uriParts[2] ?? null;
-$action = $uriParts[3] ?? null;
+$resource = $uriParts[1] ?? null;
+$action = $uriParts[2] ?? null;
 $body = json_decode(file_get_contents("php://input"), true) ?? [];
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+$controller = new HomeController();
+$isLogged = $controller->isLogged();
+$userRoleService = new UserRolesService();
+
+$adminRoutes = [
+    'articles',
+    'categories',
+    'users',
+    'roles'
+
+
+];
+$user = $_SESSION["user"] ?? NULL;
+if ($user) {
+    if (!$userRoleService->hasAdminRole($user) && in_array($resource, $adminRoutes)) {
+        $uri = ROOT . '/notFound'; // Send to 404.php because that URL does not exist
+    }
+}
+
 
 switch ($uri) {
     case ROOT . "/articles":
@@ -31,40 +54,44 @@ switch ($uri) {
         $controller->index();
         break;
     case ROOT . "/home":
-        require __DIR__ . '/../controllers/UserController.php';
-        $controller = new UserController();
-        $controller->index();
+        if ($isLogged) {
+            require __DIR__ . '/../controllers/UserController.php';
+            $userController = new UserController();
+            $userController->index();
+        } else {
+            $controller->index();
+        }
         break;
     case ROOT . "/register":
-        require __DIR__ . '/../controllers/HomeController.php';
-        $controller = new HomeController();
         $controller->register();
         break;
     case ROOT . "/login":
-        require __DIR__ . '/../controllers/HomeController.php';
-        $controller = new HomeController();
         $controller->login();
         break;
     case ROOT . "/logout":
-        require __DIR__ . '/../controllers/UserController.php';
-        $controller = new UserController();
-        $controller->logout();
+        if ($isLogged) {
+            require __DIR__ . '/../controllers/UserController.php';
+            $userController = new UserController();
+            $userController->logout();
+        } else {
+            header("Location: " . ROOT);
+        }
         break;
     case ROOT . "/confirm-email":
-        require __DIR__ . '/../controllers/HomeController.php';
-        $controller = new HomeController();
         $controller->confirmEmail();
         break;
     case ROOT . "/email/confirm":
-        require __DIR__ . '/../controllers/HomeController.php';
-        $controller = new HomeController();
         $controller->emailConfirmed();
         break;
     case ROOT:
     case ROOT . "/":
-        require __DIR__ . '/../controllers/HomeController.php';
-        $controller = new HomeController();
-        $controller->login();
+        if ($isLogged) {
+            require_once __DIR__ . '/../controllers/UserController.php';
+            $userController = new UserController();
+            $userController->index();
+        } else {
+            $controller->index();
+        }
         break;
 
     default:
